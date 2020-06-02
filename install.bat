3574815bcbf7ec8b543bb1835d333bc9491a8c03
Jesper Stefansson <grouse.jst@gmail.com>
2020-06-02 19:45:30 +0200

    add a stupid install bat that makes my life a miniscule amount easier

---
+++ install.bat
@@ -0,0 +1,13 @@
+@echo off
+
+SET ROOT=%~dp0
+SET BUILD_DIR=%ROOT%\build
+SET CUSTOM_DIR=%ROOT%\custom
+SET INSTALL_DIR=D:\apps\4coder
+
+pushd %BUILD_DIR%
+xcopy /Y /F custom_4coder.dll %INSTALL_DIR%\
+xcopy /Y /F custom_4coder.pdb %INSTALL_DIR%\
+xcopy /Y /F vc140.pdb %INSTALL_DIR%\
+xcopy /Y /F themes\theme-grouse.4coder %INSTALL_DIR%\themes\
+popd
