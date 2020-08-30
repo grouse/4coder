4197e5b3db74ee29cdcfcb93d50bd4f9b53b67bd
Jesper Stefansson <grouse.jst@gmail.com>
2020-08-30 21:09:39 +0200

    install.bat now builds the editor in release mode before copying over the files

--- build.bat
+++ build.bat
@@ -8,9 +8,8 @@ if not exist %BUILD_DIR% mkdir %BUILD_DIR%
 if not exist %BUILD_DIR%\themes mkdir %BUILD_DIR%\themes
 if not exist %BUILD_DIR%\fonts mkdir %BUILD_DIR%\fonts
 
-
 pushd %BUILD_DIR%
-call %CUSTOM_DIR%\bin\buildsuper_x64.bat %ROOT%\custom.cpp release
+call %CUSTOM_DIR%\bin\buildsuper_x64.bat %ROOT%\custom.cpp debug
 xcopy /Y /F %ROOT%\themes %BUILD_DIR%\themes
 xcopy /Y /F %ROOT%\fonts %BUILD_DIR%\fonts
 xcopy /Y /F %ROOT%\config.4coder %BUILD_DIR%\
--- install.bat
+++ install.bat
@@ -5,7 +5,17 @@ SET BUILD_DIR=%ROOT%\build
 SET CUSTOM_DIR=%ROOT%\custom
 SET INSTALL_DIR=D:\apps\4coder
 
+if not exist %BUILD_DIR% mkdir %BUILD_DIR%
+if not exist %BUILD_DIR%\themes mkdir %BUILD_DIR%\themes
+if not exist %BUILD_DIR%\fonts mkdir %BUILD_DIR%\fonts
+
+
 pushd %BUILD_DIR%
+call %CUSTOM_DIR%\bin\buildsuper_x64.bat %ROOT%\custom.cpp release
+xcopy /Y /F %ROOT%\themes %BUILD_DIR%\themes
+xcopy /Y /F %ROOT%\fonts %BUILD_DIR%\fonts
+xcopy /Y /F %ROOT%\config.4coder %BUILD_DIR%\
+
 xcopy /Y /F custom_4coder.dll %INSTALL_DIR%\
 xcopy /Y /F custom_4coder.pdb %INSTALL_DIR%\
 xcopy /Y /F vc140.pdb %INSTALL_DIR%\
